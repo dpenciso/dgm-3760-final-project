1eed8e253103b6f4935ebdbfe838d795defe5106
David Enciso <dpenciso@msn.com>
2020-12-03 12:30:33 -0700

    couldn't figure out post

--- src/script.js
+++ src/script.js
@@ -5,9 +5,7 @@ const submitButton = document.querySelector(".custom-submit");
 const ratingNum = 5;
 
 
-
-submitButton.addEventListener("click", (event) => {
-  event.preventDefault();
+async function submitEntry() {
   const data = {
     title: entryTitle.value,
     entry: entryContent.value,
@@ -15,57 +13,21 @@ submitButton.addEventListener("click", (event) => {
     rating: ratingNum,
     date: Date.now,
   };
-  console.log(data);
+  console.log(data)
+  await fetch('http://localhost:3000/entries', {
+      method: 'POST',
+      headers: { 'Access-Control-Allow-Orgin': 'Content-Type', 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+  }).then(response => response.json())
+  .then(data => {
+      console.log(data)
+  })
+}
 
-  // fetch("http://localhost:3000/entries", {
-  //   method: "POST",
-  //   headers: {
-  //     "Access-Control-Allow-Orgin": "Content-Type",
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(data),
-  // })
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     console.log("Success:", data);
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error:", error);
-  //   });
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  submitEntry()
 });
 
-// postData("https://example.com/answer", { answer: 42 }).then((data) => {
-//   console.log(data); // JSON data parsed by `data.json()` call
-// });
 
-// getRating();
 
-// function getRating() {
-//   var ele = document.getElementsByName("exampleRadios");
-
-//   for (i = 0; i < ele.length; i++) {
-//     if (ele[i].checked) {
-//       rating = ele[i].value;
-//     }
-//   }
-// }
-
-// let data = {
-//   title: entryTitle,
-//   entry: entryContent,
-//   grateful: gratefulFor,
-//   rating: rating,
-//   date: Date.now,
-// };
-
-// submitButton.addEventListener("click", (event) => {
-//   console.log(event);
-//   fetch("http://localhost:3000/entries", {
-//     method: "POST",
-//     headers: {
-//       "Access-Control-Allow-Orgin": "Content-Type",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   }).then(console.log("Success"));
-// });
