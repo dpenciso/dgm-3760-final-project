f5ba928e389702d0ba092c30a55d5daddac2e759
David Enciso <dpenciso@msn.com>
2020-12-05 19:45:29 -0700

    display past entries

--- src/script.js
+++ src/script.js
@@ -24,9 +24,40 @@ function submitEntry() {
 }
 
 submitButton.addEventListener("click", (event) => {
-  event.preventDefault();
   submitEntry()
 });
 
+async function getEntries() {
+  let url = '/entries';
+  try {
+      let res = await fetch(url);
+      return await res.json();
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+async function renderEntries() {
+  let entries = await getEntries();
+  let sortedEntries = entries.reverse()
+  let html = '';
+  sortedEntries.forEach(entry => {
+      let htmlSegment = `<div class="card custom-card" style="width: 18rem;">
+      <div class="card-body">
+        <h3 class="card-title">${entry.title}</h3>
+        <h5 class="card-title">${entry.date.slice(0,10)}</h5>
+        <p class="card-text">${entry.entry.slice(0,50)}...</p>
+        <h6 class="card-subtitle mb-2 text-muted">Rating: ${entry.rating}</h6>
+        <a href="#" class="card-link">View/Edit</a>
+      </div>
+    </div>`;
+      html += htmlSegment;
+  });
+
+  let loadedEntries = document.querySelector('.loadedEntries');
+  loadedEntries.innerHTML = html;
+}
+
+renderEntries();
 
 
